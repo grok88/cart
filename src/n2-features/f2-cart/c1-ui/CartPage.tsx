e866d76f418991e1404fff172b244b6804cba574
Alex <grok88@tut.by>
2020-10-31 22:18:06 +0300

    added TotalPrice display to CartPage.tsx

--- src/n2-features/f2-cart/c1-ui/CartPage.tsx
+++ src/n2-features/f2-cart/c1-ui/CartPage.tsx
@@ -3,15 +3,21 @@ import {UserInfo} from "./userInfo/UserInfo";
 import {Cart} from "./cart/Cart";
 import {useSelector} from 'react-redux';
 import {cartsSelector} from '../c2-bll/selectors';
-// import { filterCarts } from '../c2-bll/selectors';
 
 export const CartPage = React.memo(() => {
     const carts = useSelector(cartsSelector);
-    console.log(carts)
-    return (<div style={{display:'flex'}}>
+
+    const TotalPrice = carts.reduce((acc, cart) => {
+        return acc + cart.count * cart.price
+    }, 0);
+
+    return (<div>
         {
             carts.map(cart => <Cart cart={cart} key={cart.id}/>)
         }
         <UserInfo/>
+        <div>
+            Total: {TotalPrice} $
+        </div>
     </div>);
 });
